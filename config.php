6c41d331a247e370caeafd99cb349e892e0907dc
David Cooke <cookedavid637@gmail.com>
2019-02-16 00:11:32 +0000

    cleanup some code

--- config.php
+++ config.php
@@ -7,5 +7,3 @@ define('MYSQL_DATABASE', 'database');
 define('TWITTER_USERNAME', 'drcooke_');
 define('SHORT_URL', 'https://dave.lc/'); // include the trailing slash!
 define('DEFAULT_URL', 'https://drcooke.co.uk'); // omit the trailing slash!
-
-?>
