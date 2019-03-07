450d30b57b2dbb218cb44fae8f605f2d1c75cbe4
FriendlyFire <jpguertin.cg@gmail.com>
2019-03-07 00:23:46 -0500

    1000 bytes limit on key.

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 # Why you should use `utf8mb4` instead of `utf8`: http://mathiasbynens.be/notes/mysql-utf8mb4
 CREATE TABLE `redirect` (
-	`slug` varchar(255) collate utf8mb4_unicode_ci NOT NULL,
+	`slug` varchar(250) collate utf8mb4_unicode_ci NOT NULL,
 	`url` varchar(620) collate utf8mb4_unicode_ci NOT NULL,
 	`date` datetime NOT NULL,
 	`hits` bigint(20) NOT NULL default '0',
